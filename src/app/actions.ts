1ae35fe9ec187066a80f2def3acbb7a52989a6a4
Shina Emmanuel Akeredolu <shinaemmanuelakeredolu@gmail.com>
2025-11-29 16:07:19 +0000

    fix this error

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -61,7 +61,7 @@ export async function registerSeedPhrase(
     };
   }
 
-  const supabaseUrl = process.env.SUPABASE_URL;
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
   const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
   if (!supabaseUrl || !supabaseServiceRoleKey) {
